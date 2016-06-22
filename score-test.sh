e787cd70e86e5732a0c24fe2a922ea38437362e4
Andy Camara <acamara86@gmail.com>
2016-06-21 20:24:38 -0400

    updated with decimal point change

--- score-test.sh
+++ score-test.sh
@@ -2,7 +2,7 @@
 
 x=5; y=100; b=2;
 
-seq=$(/bin/echo "$y / $x" | /usr/bin/bc -l)
+seq=$(/bin/echo "scale=0; $y / $x" | /usr/bin/bc -l)
 total=$(/bin/echo "$seq * $b" | /usr/bin/bc -l)
 
 echo "$seq and $total"
